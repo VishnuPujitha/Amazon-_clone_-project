7d6615cdc1266ab417e19b1139bebd527c19bf81
VishnuPujitha <153887300+VishnuPujitha@users.noreply.github.com>
2024-06-24 16:11:02 +0530

    move addtocart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
  export const cart = [];//this variable can we used outside of cart
+ export function addToCart(productId){
+  let matchingItem;
+  cart.forEach((cartItem)=>{
+    if (productId === cartItem.productId){
+      matchingItem =cartItem;
+
+    }
+  });
+  if (matchingItem){
+    matchingItem.quanity+=1;
+
+  }
+  else{
+    cart.push({
+      productId:productId,
+      quantity:1
+  });
+
+  }
+
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
 //generating the html in js
-import{cart} from'../data/cart.js';
+import{cart,addToCart} from'../data/cart.js';
 import{products} from'../data/products.js';
 let productsHTML= '';
 products.forEach((product)=>{
@@ -154,34 +154,26 @@ products.forEach((product)=>{
 
 });
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
+//the below function handles upadting the web page 
+
+function updateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((cartItem)=>{
+    cartQuantity +=cartItem.quantity;
+
+  });
+  document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+}
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
     button.addEventListener('click',()=>{
       const productId = button.dataset.productId;
-      let matchingItem;
-      cart.forEach((item)=>{
-        if (productId === item.productId){
-          matchingItem =item;
-
-        }
-      });
-      if (matchingItem){
-        matchingItem.quanity+=1;
-
-      }
-      else{
-        cart.push({
-          productId:productId,
-          quantity:1
-      });
-
-      }
-      let cartQuantity = 0;
-      cart.forEach((item)=>{
-        cartQuantity +=item.quantity;
-
-      });
-      document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+      addToCart(productId);
+      updateCartQuantity();
+     
+     
+    
      
 
       
