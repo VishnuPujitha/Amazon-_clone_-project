88fc27bb01d56c0f1e1b7f22e6a4eb2a8e0a0145
VishnuPujitha <153887300+VishnuPujitha@users.noreply.github.com>
2024-06-25 12:39:44 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,20 @@
- export let cart = [{
+ export let cart = JSON.parse(localStorage.getItem('cart'));
+ if(!cart){
+
+  cart=[{
+
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,//saving the data for checkout js
  },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
- }];//this variable can we used outside of cart
+ }];
+ }
+
+ 
+ function saveTostorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+ }
  export function addToCart(productId){
   let matchingItem;
   cart.forEach((cartItem)=>{
@@ -24,6 +34,7 @@
   });
 
   }
+  saveTostorage();
 
 }
  export function removeFromCart(productId){
@@ -38,4 +49,5 @@
 
   });
   cart =newCart;
+  saveTostorage();
 }
