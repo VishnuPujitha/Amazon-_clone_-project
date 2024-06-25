8f1e2c0aceae4aede7ba60d82ec660b2100389f6
VishnuPujitha <153887300+VishnuPujitha@users.noreply.github.com>
2024-06-25 11:15:12 +0530

    generate html for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
- export const cart = [];//this variable can we used outside of cart
+ export const cart = [{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2,//saving the data for checkout js
+ },{
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity:1
+ }];//this variable can we used outside of cart
  export function addToCart(productId){
   let matchingItem;
   cart.forEach((cartItem)=>{
